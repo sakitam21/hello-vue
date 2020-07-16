185630c207198dde82f12c3dbf0a212e938ce6a8
sakitam21 <1983409794@qq.com>
2020-07-17 00:12:51 +0800

    commit

--- src/components/main/Footer.vue
+++ src/components/main/Footer.vue
@@ -1,6 +1,100 @@
 <template>
   <div class="footer">
-    <h1>Footer</h1>	
+    <div class="footerItem">
+      <div class="title">Tldzal pfen qebmghvgn citghyi</div>
+      <div class="content">
+        <ul>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+        </ul>
+      </div>
+    </div>
+    <div class="footerItem">
+      <div class="title">Tldzal pfen qebmghvgn citghyi</div>
+      <div class="content">
+        <ul>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+        </ul>
+      </div>
+    </div>
+    <div class="footerItem">
+      <div class="title">Tldzal pfen qebmghvgn citghyi</div>
+      <div class="content">
+        <ul>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+        </ul>
+      </div>
+    </div>
+    <div class="footerItem">
+      <div class="title">Tldzal pfen qebmghvgn citghyi</div>
+      <div class="content">
+        <ul>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+          <li>Jenin</li>
+          <div class="divide"></div>
+          <li>Hsbuu</li>
+          <div class="divide"></div>
+          <li>Tesie</li>
+          <div class="divide"></div>
+          <li>Iqsys</li>
+          <div class="divide"></div>
+        </ul>
+      </div>
+    </div>
+    <div class="bottom">
+      bottom
+    </div>
   </div>
 </template>
 
@@ -12,12 +106,56 @@ export default{
 
 <style scoped>
 .footer{
-	height: 60px;
+	position: relative;
+	height: 620px;
 	width: 100%;
-	background-color: #999;
-	margin: 20px 0;
+	margin-top: 30px;
+	background-color: #333838;
+}
+.footer .footerItem{
+	height: 60px;
+	padding: 20px 0;
+	margin: 0 40px;
+	border-bottom: 1px solid #fff;
+	color: #f5f5f5;
+	font-size: 18px;
+}
+.footer .footerItem .title{
+	height: 60px;
+	line-height: 30px;
+	padding: 0 30px;
+	width: 24%;
+	float: left;
+}
+.footer .footerItem .content{
+	height: 60px;
+	line-height: 30px;
+	width: 60%;
+	float: left;
 	text-align: center;
-	line-height: 60px;
-	color: #f3f3f3;
+}
+.content li{
+	display: inline-block;
+	margin: 0 30px;
+	height: 30px;
+	float: left;
+}
+.content .divide{
+	width: 2px;
+	height: 16px;
+	margin-top: 7px;
+	background-color: #ccc;
+	float: left;
+}
+
+.bottom{
+	position: absolute;
+	bottom: 0px;
+	left: 0px;
+	height: 200px;
+	line-height: 200px;
+	text-align: center;
+	background-color: #f5f5f5;
+	width: 100%;
 }
 </style>
